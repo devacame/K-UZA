cb6641c940911d0df9914b39ea3ddd65ff9dccbb
coder38611 <54740236+coder38611@users.noreply.github.com>
2021-04-10 13:51:37 +0900

    Fixed Translation Functionality

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from setup_google_storage import create_bucket, upload_audio_file, delete_audio_file
 from youtube_download import get_youtube_audio
 from caption_stt import run_stt
+from caption_text_translate import translate
 from google.cloud import storage
 from os import environ
 environ['GOOGLE_APPLICATION_CREDENTIALS'] = './GCP-API-KEY.json'
@@ -25,10 +26,11 @@ def run_backend(url, lang='en-US'):
         caption = run_stt(lang, bucket_name)
         if caption.startswith('Fatal'):
             return caption
+        translated_text = translate(caption, lang)
     except Exception:
         return 'Fatal:알 수 없는 에러가 발생하였습니다.'
     finally:
         deletion_state = delete_audio_file(bucket_name)
         if deletion_state:
             return deletion_state
-    return caption
+    return translated_text
--- src/caption_text_translate.py
+++ src/caption_text_translate.py
@@ -1,15 +1,29 @@
+from google.cloud import translate
 from google.cloud import translate_v2 as translate
-import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/kimtaeyi/python_workspace/KUZA-DEV-a933fc24c14d.json'
-
-def trans(text, target_lang):
-    try:
-        client = translate.Client()
-        result = client.translate(text, target_lang)
-    except:
-        return None
-
-    return result['translatedText']
 
 
-
+def translate(text, source_lang, target_language='ko'):
+    if source_lang == None:
+        try:
+            client = translate.Client()
+            result = client.translate(text, target_language)
+            return (result['translatedText'])
+        except:
+            return None
+    else:
+        try:
+            response = client.translate_text(
+                request={
+                    "parent": parent,
+                    "contents": [text],
+                    "mime_type": "text/plain",
+                    "source_language_code": source_lang,
+                    "target_language_code": target_language,
+                }
+            )
+            word_translate = []
+            for translation in response.translations:
+                word_translate.append(translation.translated_text)
+            return " ".join(word_translate)
+        except:
+            return None
